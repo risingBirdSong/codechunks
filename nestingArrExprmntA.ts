ccb94e7b157ec16bd84f0618c18352a8547cda71
risingBirdSong <52929863+risingBirdSong@users.noreply.github.com>
2020-01-04 19:29:09 -0800

    Update nestingArrExprmntA.ts

--- nestingArrExprmntA.ts
+++ nestingArrExprmntA.ts
@@ -1,8 +1,9 @@
 function  mapNumbersTest (arr : number[], times : number) {
   let outer = new Array(times).fill(0);
     return outer.map((x,idx,thearr)=>{
-        return thearr.map((y, i, innerarr)=>{
+        return arr.map((y, i, innerarr)=>{
             return y = idx;
         })
     })
 }
+
